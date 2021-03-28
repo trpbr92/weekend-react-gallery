4a84061178d34d62a1c26c90c599746c2b4a8dfe
Trace Brandt <trpbr92@gmail.com>
2021-03-28 17:07:10 -0500

    conditional rendering working, not replacing photo

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,10 +1,26 @@
 import './GalleryItem.css';
 import axios from 'axios';
+import {useState} from 'react';
 
-
+ 
 
 
 function GalleryItem(props){
+
+     const [showDescription, setShowDescription] = useState(false);
+
+     let toggleName = ()=>{
+         setShowDescription(!showDescription);
+         console.log('in toggleName', showDescription);
+     }
+
+     let displayDescription = ()=>{
+         let displayText = '';
+         if (showDescription) {
+             displayText = props.item.description
+         }
+         return displayText;
+     }
 
     const GalleryLike = ()=>{
         let id = props.item.id;
@@ -19,8 +35,8 @@ function GalleryItem(props){
 
     return(
         <>
-        <img src={props.item.path} className="image"></img>
-        <div>DESCRIPTION: {props.item.description} <button onClick={GalleryLike}>Like</button> {props.item.likes}</div>
+        <img src={props.item.path} className="image" onClick={toggleName}></img>
+        <div>{displayDescription()} <button onClick={GalleryLike}>Like</button> {props.item.likes} People like this! </div>
         </>
     )
 }
