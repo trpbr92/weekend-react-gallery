843a4496ee66eaf95879815dbdbaed9f55fd4e62
Trace Brandt <trpbr92@gmail.com>
2021-03-28 08:07:57 -0500

    props showing description and likes

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,8 +1,7 @@
 function GalleryItem(props){
     return(
         <>
-        <h1>Gallery Item</h1>
-        <p>{JSON.stringify(props)}</p>
+        <li>{props.item.description}, {props.item.likes}</li>
         </>
     )
 }
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -6,7 +6,7 @@ function GalleryList(props){
         <h1>Gallery List</h1>
         <ul>
           {props.photos.map((item, index)=>
-              <GalleryItem item={item}/>
+              <GalleryItem item={item} key={index}/>
           )}  
         </ul>
         </>
