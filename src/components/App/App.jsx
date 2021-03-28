85f2bca0a2d7ebe9b2ef27cf4d54340b159b910d
Trace Brandt <trpbr92@gmail.com>
2021-03-28 09:46:51 -0500

    images resized

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -27,9 +27,9 @@ const GetGallery = ()=>{
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
+        <div className="gallery">
         <GalleryList photos={photos}/>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        </div>
       </div>
     );
 }
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,7 +1,9 @@
+import './GalleryItem.css';
+
 function GalleryItem(props){
     return(
         <>
-        <img src={props.item.path}></img>
+        <img src={props.item.path} className="image"></img>
         <div>DESCRIPTION: {props.item.description} LIKES: {props.item.likes}</div>
         </>
     )
